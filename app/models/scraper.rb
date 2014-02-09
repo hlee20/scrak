1b43d10bee4ab6f6b60a812e3b7d4d6a696bfe38
HeeSang Lee <HeeSang@heesangs-mbp.risd.edu>
2014-02-09 16:29:11 -0500

    my first commit

---
+++ app/models/scraper.rb
@@ -0,0 +1,22 @@
+class Scraper
+def self.scrape
+		data =
+			HTTParty.get(
+			'http://www.brainyquote.com/quotes/authors/a/albert_einstein.html')
+
+
+		response_body =
+			data.response.body
+
+		doc = Nokogiri::HTML(
+			response_body
+		)
+
+		quotes = []
+		doc.css('.bqQuoteLink a').each do |link|
+			quotes << link.content
+		end
+
+		quotes.sample
+		end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scrak::Application.config.secret_key_base = '3077845c1c3860cc66810e07c6427b64a31dc94ec66e55153aff9b1b890cb46c732b7eceba13441adbb2f46dba7e1e3208702b6436018d58556c57c1fc7130aa'
